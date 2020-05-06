d12d3b276e70e97038a95317576a4b5a7b249749
Allison Vanoy <allison_vanoy@hotmail.com>
2020-05-06 17:35:57 -0400

    fixed script for upvote and downvote buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,62 @@
-//increase/decrease likes on upvote or downvote clicks
-let likesNumArr = $('.upVotes span');
-let upVoteArr = $('.thumbUp');
-let downVoteArr = $('.thumbDown');
+/*************************
+ specific happy hour voting
+*************************/
+	//increase likes on upvote
+	let upVoteArr = $('.thumbUp');
+	upVoteArr.click(function() {
+		let numLikes = $(this).closest('.happyHourSpecial').find('.voteTotal span');
+		let numLikesVal = parseInt(numLikes[0].innerText);
+		numLikes[0].innerText = numLikesVal + 1;
+	});
 
-upVoteArr.on('click', function() {
-	let likesContent = parseInt(likesNumArr[0].innerText);
-	likesNumArr[0].innerText = likesContent + 1;
+	//decrease likes on downvote
+	let downVoteArr = $('.thumbDown');
+	downVoteArr.click(function() {
+		let numLikes = $(this).closest('.happyHourSpecial').find('.voteTotal span');
+		let numLikesVal = parseInt(numLikes[0].innerText);
+		numLikes[0].innerText = numLikesVal - 1;
+	});
+/*************************
+ end voting
+*************************/
+
+
+/*************************
+add/delete another happy hour
+section in form
+*************************/
+	//add happy hour section when "add more" is clicked
+	let cloneContent = $('.happyHourContent');
+
+	$('#happyHourAdd').click(function() {
+		$(cloneContent[0]).clone() //duplicate content section
+							.find('input:text').val('').end() //clear input values
+							.find('input:checkbox').prop('checked', false).end() //clear selected checkboxes
+							.appendTo('#happyHourContainer').addClass('mt-4'); //add duplicate to parent div
+	});
+
+	//delete happy hour section when X is clicked unless it's the last one
+	let deleteBtnArr = $.find('.happyHourDelete');
+	let deleteContent = $.find('.happyHourContent');
+
+	$(deleteBtnArr).each(function(i) {
+		$(this).click(function() {
+			console.log(i);
+	//		$(deleteContent[i]).remove();
+		});
+	});
+/*************************
+end add/delete another happy hour
+*************************/
+
+
+
+//open/close login modal
+$('#loginLink').click(function() {
+	$('#loginModal').modal('show');
 });
+
+$('.close').click(function() {
+	$('#loginModal').modal('hide');
+});
+
